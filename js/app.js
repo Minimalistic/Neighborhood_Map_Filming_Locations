c5b3a8f1b1252c066dc0a0c30fb6623af1261d82
Jason Marsh <regularly.jason@gmail.com>
2017-06-15 16:22:28 -0500

    Feat: Ext database file for location and working descriptions

--- js/app.js
+++ js/app.js
@@ -1,25 +1,73 @@
-      var map;
-      function initMap() {
-        // Constructor creates a new map - only center and zoom are required.
-        // This map is centered over Sanibel Island in Florida.
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 26.427688, lng: -82.10251},
-          zoom: 13
-        });
-        var sanibel_sprout = {lat: 26.434856, lng: -82.0791256};
-        var marker = new google.maps.Marker({
-          position: sanibel_sprout,
-          map: map,
-          title: 'First Marker!!',
-          animation: google.maps.Animation.DROP,
-          draggable: true,
-          title: "This marker is draggable!"
-        });
-        var infowindow = new google.maps.InfoWindow({
-          content: 'Do you ever feel like an InfoWindow?' +
-            ' ready to start again?'
-        });
-        marker.addListener('click', function() {
-          infowindow.open(map, marker);
-        });
-      }
+var map;
+// Create a new blank array for all the listing markers.
+var markers = [];
+function initMap() {
+// Constructor creates a new map - only center and zoom are required.
+map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 26.454856, lng: -82.0791250},
+    zoom: 13
+});
+
+function showListings() {
+    var bounds = new google.maps.LatLngBounds();
+    // Extend boundaries of map for markers and display
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+        bounds.extend(markers[i].position);
+    }
+    map.fitBounds(bounds);
+}
+
+function hideListings() {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+}
+var largeInfowindow = new google.maps.InfoWindow();
+var bounds = new google.maps.LatLngBounds();
+// The following group uses the location array to create an array of markers on initialize.
+for (var i = 0; i < locations.length; i++) {
+    // Get the position from the location array.
+    var position = locations[i].location;
+    var title = locations[i].title;
+    var description = locations[i].description;
+    // Create a marker per location, and put into markers array.
+    var marker = new google.maps.Marker({
+        position: position,
+        title: title,
+        description: description,
+        animation: google.maps.Animation.DROP,
+        id: i
+  });
+  // Push the marker to our array of markers.
+  markers.push(marker);
+  // Create an onclick event to open an infowindow at each marker.
+  marker.addListener('click', function() {
+    populateInfoWindow(this, largeInfowindow);
+    });
+    bounds.extend(markers[i].position);
+}
+
+document.getElementById('show-listings').addEventListener('click', showListings);
+document.getElementById('hide-listings').addEventListener('click', hideListings);
+}
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+// Check to make sure the infowindow is not already opened on this marker.
+if (infowindow.marker != marker) {
+    infowindow.marker = marker;
+    infowindow.setContent('<ul>' + '<h2>' + marker.title + '</h2>' + 
+                                    '<h4>' + 'Description' + '</h4>' +
+                                    marker.description +
+                                   '<h4>Latitude and Longitude</h4>' + 
+                                    marker.position + 
+                          '</ul>');
+    infowindow.open(map, marker);
+    // Make sure the marker property is cleared if the infowindow is closed.
+    infowindow.addListener('closeclick',function(){
+        infowindow.setMarker = null;
+    });
+    }
+}
---
+++ js/locations.js
@@ -0,0 +1,20 @@
+var locations = [
+    {title: 'Sanibel Sprout', 
+        location: {lat: 26.434856, lng: -82.0791256},
+        description: 'Test Description'},
+    {title: 'Great Shelling Beach',
+        location: {lat: 26.457203, lng: -82.153015}},
+
+    {title: 'Lighthouse Beach',
+        location: {lat: 26.452853, lng: -82.014549}},
+
+    {title: 'The Island Cow',
+        location: {lat: 26.437499, lng: -82.071067}},
+
+    {title: 'She Sells Sea Shells',
+        location: {lat: 26.436257, lng: -82.078388}},
+
+    {title: 'Red Mangrove Island',
+        location: {lat: 26.429486, lng: -82.083688}}
+
+];
